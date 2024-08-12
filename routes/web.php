284463b0b68e00f44289ea84905540a61d89e49a
Sergio Victoria <programador.sensacionrural@gmail.com>
2024-08-12 12:53:29 +0200

    Rutas web principales

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+Route::get('/projects', function () {
+    return view('projects');
+})->name('projects');
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
