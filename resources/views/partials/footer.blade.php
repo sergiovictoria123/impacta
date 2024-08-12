6f8792025cce073a2d776da7fa37d8517ad4a146
Sergio Victoria <programador.sensacionrural@gmail.com>
2024-08-12 12:52:31 +0200

    Diseño de footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,63 @@
+<div class="container-fluid p-4 pb-0">
+    <div class="row mt-3">
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <p class="text-primary">Contacto y Preguntas Frecuentes</p>
+            <ul class="nav flex-column text-light">
+                <li>+ (34) 666 666 666</li>
+                <li>FORMULARIO DE CONTACTO</li>
+                <li>PREGUNTAS FRECUENTES</li>
+                <li>LIBRO DE RECLAMACIONES</li>
+                <li>CONOCE NUESTRA MANERA DE TRABAJAR</li>
+            </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <p class="text-primary">Nosotros</p>
+            <ul class="nav flex-column text-light">
+                <li>CATEGORIAS</li>
+                <li>NOVEDADES</li>
+                <li>OFERTAS</li>
+            </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <p class="text-primary">Proyectos</p>
+            <ul class="nav flex-column text-light">
+                <li>CALIDADES</li>
+                <li>REVISIONES</li>
+                <li>RENOVACIÓN</li>
+                <li>VENTAS</li>
+            </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <p class="text-primary">Contacto</p>
+            <ul class="nav flex-column text-light">
+                <li>FORMULARIO DE CONTACTO</li>
+            </ul>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-lg-6 col-md-6 mb-4 mb-md-0">
+            <p class="text-primary text-center">Servicios</p>
+            <p class="text-light">Pago Seguro</p>
+            <div class="list-group d-flex align-items-start gap-3">
+                <ul class="list-inline mb-0">
+                    <li class="list-inline-item"><img src="images/logos/mastercard.svg" alt="mastercard" class="logo-footer"></li>
+                    <li class="list-inline-item"><img src="images/logos/visa.svg" alt="visa" class="logo-footer"></li>
+                    <li class="list-inline-item"><img src="images/logos/americanexp.svg" alt="americanexp" class="logo-footer"></li>
+                    <li class="list-inline-item"><img src="images/logos/paypal.svg" alt="paypal" class="logo-footer"></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 mb-4 mb-md-0">
+            <p class="text-primary text-center">Redes</p>
+            <div class="list-group d-flex align-items-center gap-3">
+                <ul class="list-inline mb-0">
+                    <li class="list-inline-item"><img src="images/logos/instagram.svg" alt="logo instagram footer" class="logo-redes-footer"></li>
+                    <li class="list-inline-item"><img src="images/logos/facebook.svg" alt="logo facebook footer" class="logo-redes-footer"></li>
+                </ul>
+            </div>
+        </div>
+        <a class="navbar-brand d-flex justify-content-end" href="{{ route('welcome') }}">
+            <img src="{{ asset('storage/images/logos/impacta2.svg') }}" alt="Logo Impacta" style="height: 20px;">
+        </a>    
+    </div>
+</div>
