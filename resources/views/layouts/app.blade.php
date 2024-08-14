cb3d242b3672806ebfba47ec219c23a718ff1361
Sergio Victoria <programador.sensacionrural@gmail.com>
2024-08-14 18:30:26 +0200

    Agrego favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ asset('storage/images/iconos/favicon.ico') }}" type="image/x-icon"/>
     <title>@yield('title', 'Impacta')</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
